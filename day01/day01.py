4be68c0e5ca5da7808d66eeb167385760308bcaa
bneb <bneb@users.noreply.github.com>
2020-11-30 23:45:25 -0800

    day1 done! 😎

---
+++ day01/day01.py
@@ -0,0 +1,36 @@
+'''Day 01 of Advent of Code 2020
+
+The prompt is that a sub list of integers from the input should sum to a target
+value, in this case 2020, and that we want to return the product of the numbers
+in that sublist.
+'''
+
+from argparse import ArgumentParser
+from functools import reduce
+from itertools import combinations
+from pathlib import PurePath
+
+def day(data, n, target_number):
+    '''With a list of numbers, how many numbers to use (n), and a target sum:
+    return the product of a combination of n numbers that sum to the target.
+    '''
+    for combo in combinations(data, n):
+        if sum(combo) == target_number:
+            return reduce(lambda x, y: x*y, combo)
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description=day.__doc__)
+    parser.add_argument('-f', type=PurePath, help='the input file')
+    parser.add_argument('-n', type=int, default=2, help='use this many numbers')
+    parser.add_argument('-t', type=int, default=2020, help='the target number')
+    args = parser.parse_args()
+    
+    with open(args.f, 'r') as f:
+        data = [int(line.strip()) for line in f.readlines()]
+
+    n = args.n
+    solution = day(data, n, args.t)
+
+    print("Day 01 solution: {}".format(solution))
+
