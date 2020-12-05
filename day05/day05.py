f993209819ca588b818e856ee742e6b03941e4a3
bneb <bneb@users.noreply.github.com>
2020-12-05 11:49:30 -0800

    cleanup day05

--- day05/day05.py
+++ day05/day05.py
@@ -17,6 +17,8 @@ from argparse import ArgumentParser
 from pathlib import PurePath
 
 def main(filepath):
+    '''Given an input filepath, load input and produce solutions.
+    '''
     with open(filepath, 'r') as f:
         data = [line.strip() for line in f.readlines()]
 
@@ -37,9 +39,9 @@ def part1(data):
     
 
 def part2(data):
-    '''All seats not at the very beginning or end are full.
+    '''Given a list of boarding passes, find the only missing value.
 
-    Using process of elimination, find your seat.
+    One seat between the first and last seat id is not present. Return it.
     '''
     seat_set = set(get_seat_numbers(data))
     min_seat = min(seat_set)
@@ -53,15 +55,23 @@ def part2(data):
 
 
 def get_seat_numbers(data):
+    '''For each seat string, compute the seat id and return the list
+    '''
     return map(get_seat_id, data)
 
 
 def get_seat_id(seat):
+    '''Seat ID is the 8*row + col
+    '''
     row, col = bin_cast(seat)
     return 8*row+col
 
 
 def bin_cast(boarding_pass):
+    '''Given a boarding pass string, return the row and col as ints
+
+    Convert the strs to binary ints: first 7 are the row, last 3 are the column.
+    '''
     row = int(boarding_pass[:7].replace('B', '1').replace('F', '0'), 2)
     col = int(boarding_pass[7:].replace('R', '1').replace('L', '0'), 2)
 
@@ -71,10 +81,6 @@ def bin_cast(boarding_pass):
 if __name__ == '__main__':
     parser = ArgumentParser(description=main.__doc__)
     parser.add_argument('-f', type=PurePath, help='the input file')
-    parser.add_argument('-p', type=int, default=1, help='which part to solve')
     args = parser.parse_args()
 
-    part = args.p
-    filepath = args.f
-
-    main(filepath)
+    main(args.f)
