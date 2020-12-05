3018f4e0798fdc8527afe1c044548e82d441d9cd
bneb <bneb@users.noreply.github.com>
2020-12-04 20:49:18 -0800

    simplify script generation

--- new_day.py
+++ new_day.py
@@ -5,21 +5,30 @@ Usage:
 '''
 
 from argparse import ArgumentParser
+from glob import glob
 from pathlib import Path
 import os
 
 if __name__ == '__main__':
-    files = os.listdir(os.path.dirname(os.path.realpath(__file__)))
-    day = sum([1 for f in files if f[:3] == 'day']) + 1
+    # The next few lines glob directories to find the first day not to exist
+    directory = os.path.dirname(os.path.realpath(__file__))
+    path_pattern = os.path.join(directory, 'day*')
 
+    day = 0
+    for i, f in enumerate(sorted(glob(path_pattern))):
+        day = i+1
+        if int(f[-2:]) != i+1:
+            break
+
+    # Default to the latest day that has yet to be done
     parser = ArgumentParser(description='Create daily challenge boilerplate.')
     parser.add_argument('-d', type=int, default=day, help='the puzzle day')
     args = parser.parse_args()
+
+    # Overwrite if a day argument has been passed from the command line
     day = args.d
-    
     path = Path('./day{:0>2}'.format(day))
     path.mkdir()
-
 
     # write stub of python file
     python_path = path / 'day{:0>2}.py'.format(day)
@@ -53,7 +62,9 @@ if __name__ == '__main__':
 '''.format(day=day)
         )
 
-    # create input file
-    input_path = path / 'day{:0>2}_input.txt'.format(day)
+    # create input and test input files
+    input_path = path / 'input.txt'.format(day)
     input_path.touch()
+    test_input_path = path / 'test_input.txt'.format(day)
+    test_input_path.touch()
 
