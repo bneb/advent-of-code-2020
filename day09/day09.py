816f0027dc98d101cef9e0c9692712f29f8c527d
bneb <bneb@users.noreply.github.com>
2020-12-08 21:44:07 -0800

    use better algorithm for part 2

--- day09/day09.py
+++ day09/day09.py
@@ -1,6 +1,6 @@
 '''Day 09 of Advent of Code 2020
 
-The prompt is ...
+The prompt is blah blah blah honestly who cares.
 '''
 
 from argparse import ArgumentParser
@@ -8,10 +8,10 @@ from pathlib import PurePath
 import re
 
 def main(file_path):
-    '''The docstring goes here.
+    '''Given the input filepath, parse the input and print the solutions.
     '''
     with open(file_path, 'r') as f:
-        data = [line.strip() for line in f.readlines()]
+        data = [int(line.strip()) for line in f.readlines()]
 
     solution = part1(data)
     print("Day 09 Part 1 solution: {}".format(solution))
@@ -21,32 +21,53 @@ def main(file_path):
 
 
 def part1(data):
+    '''Find the first number that isn't a sum of any two of the
+    preceding 25 numbers.
+    '''
     for i in range(25, len(data)):
-        pre = set([int(d) for d in data[i-25:i]])
-        found = False
-        target = int(data[i])
+        available_nums = set(data[i-25:i])
+        sum_target = data[i]
+        sum_found = False
 
-        for num in pre:
-            search_num = target-num
+        for num in available_nums:
+            difference = sum_target - num
 
-            if search_num in pre:
-                found = True
+            if difference in available_nums:
+                sum_found = True
 
-        if not found:
-            return target
+        if not sum_found:
+            return sum_target
+
+    raise Exception('why u do this??!?!?! 🤮')
 
 
 def part2(data):
+    '''Find a run in the data that sums to the number returned in part 1.
+
+    Return the min + max from that run.
+    '''
     target = part1(data)
 
-    for i in range(len(data)):
-        for j in range(i+2, len(data)):
+    index_start = 0
+    index_stop = 1
+    data_sum = sum(data_between(data, index_stop, index_stop))
 
-            r = [int(x) for x in data[i:j]]
-            if sum(r) == target:
-                return min(r) + max(r)
-            if sum(r) > target:
-                continue
+    while(data_sum != target):
+        if data_sum < target:
+            index_stop += 1
+        elif data_sum > target:
+            index_start += 1
+
+        data_sublist = data_between(data, index_start, index_stop)
+        data_sum = sum(data_sublist)
+
+    return min(data_sublist) + max(data_sublist)
+
+
+def data_between(data, index_start, index_stop):
+    '''Return an array of values in data between i and j inclusive.
+    '''
+    return data[index_start:index_stop+1]
 
 
 if __name__ == '__main__':
