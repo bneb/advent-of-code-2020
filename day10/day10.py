c2961ad21d025d3a5c6c5108f7b6bf3b69ebc380
bneb <bneb@users.noreply.github.com>
2020-12-09 23:55:32 -0800

    this is how to actually do this

--- day10/day10.py
+++ day10/day10.py
@@ -18,36 +18,52 @@ def main(file_path):
     solution = part1(data)
     print("Day 10 Part 1 solution: {}".format(solution))
 
-    t = time.process_time()
     solution = part2(data)
-    print('elapsed time', time.process_time(), 'for input size', len(data)-2)
     print("Day 10 Part 2 solution: {}".format(solution))
 
 
 def part1(data):
+    # count by diff between adjacent sorted values
     counter = Counter([data[i]-data[i-1] for i in range(1, len(data))])
 
+    # return the produce of the number of 1 diffs and 3 diffs
     return counter[1] * counter[3]
 
 
 def part2(data):
-    paths = {}
-    stop = max(data)
+    '''It's amazing that I've never had to solve a problem this way before.
+    Very cool. Shouts to Ajit for suggesting this 😎.
 
-    for el in data:
-        paths[el] = [x for x in data if el < x <= el + 3]
+    Intuition is that the only way to get to the end is to get to any node
+    with a value within three of the ultimate node. The same is true for each
+    of those nodes, and so on.
 
-    
-    return part2_recurse(0, paths, stop)
+    Build a table with the number of ways to get to each node (1 way to start!)
+    Then apply the logic above.
+    '''
+    # one way to start at 0
+    path_counts = {0: 1}
+
+    # set counts for each node
+    for i, node in enumerate(data[1:]):
+        print(path_counts)
+
+        # accumulate the path_counts of nodes within 3 of the current
+        num_paths = 0
+
+        # lookback at most 3, unless i-3 is out of bounds
+        lookback_index = max(i-3, 0)
+
+        for other_node in data[lookback_index:]:
+            if other_node >= node - 3:
+                num_paths += path_counts.get(other_node, 0)
+
+        path_counts[node] = num_paths
+
+    # return the path_count of the max element
+    return path_counts[max(data)]
 
 
-def part2_recurse(cur, paths, stop):
-
-    # print('checking', cur, 'in', paths)
-    if cur == stop:
-        return 1
-    
-    return sum([part2_recurse(x, paths, stop) for x in paths[cur]])
 
 
 if __name__ == '__main__':
